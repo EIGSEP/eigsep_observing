38666ae0b23b8084d73f687a0fa8ccd91cd06636
Christian Hellum Bye <chbye@berkeley.edu>
2025-05-08 14:32:53 -0700

    add observing mode

--- eigsep_observing/io.py
+++ eigsep_observing/io.py
@@ -17,6 +17,7 @@ def data_shape(ntimes, acc_bins, nchan, cross=False):
         spec_len *= 2  # real and imaginary parts
     return (ntimes, spec_len)
 
+
 def write_file(fname, data, header):
     """
     Write data to a file.
@@ -32,6 +33,7 @@ def write_file(fname, data, header):
 
     """
     raise NotImplementedError("This function is not implemented yet.")
+
 
 class File:
 
@@ -49,7 +51,7 @@ class File:
             Number of time steps to accumulate per file.
         header : dict
             Header information to be written to the file.
-        
+
         """
         self.save_dir = save_dir
         self.ntimes = ntimes
@@ -66,6 +68,7 @@ class File:
             self.data[p] = np.zeros(shape, dtype=dtype)
 
         self.acc_cnt = np.zeros(ntimes, dtype=dtype)
+        self.obs_mode = np.empty(ntimes, dtype=str)
         self._counter = 0
 
     def __len__(self):
@@ -81,7 +84,7 @@ class File:
         self.acc_cnt.fill(0)
         self._counter = 0
 
-    def add_data(self, data, cnt):
+    def add_data(self, data, cnt, mode="sky"):
         """
         Populate the data arrays with the given data.
 
@@ -91,6 +94,9 @@ class File:
             Dictionary of data arrays to be added for one time step.
         cnt : int
             Current accumulation count.
+        mode : str
+            Observing mode, either ``sky'', ``noise'', or ``load''. Added
+            to the header.
 
         Returns
         -------
@@ -103,6 +109,7 @@ class File:
             arr = self.data[p]
             arr[self._counter] = d
             self.acc_cnt[self._counter] = cnt
+            self.obs_mode[self._counter] = mode
         self._counter += 1
         if self._counter == self.ntimes:
             return self.corr_write()
@@ -124,10 +131,14 @@ class File:
             Filename where the data was written.
 
         """
+        # XXX
+        # need to write acc cnt and obs mode to the file
+        # XXX
+        :q
+        :q
         if fname is None:
             date = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
             fname = Path(self.save_dir) / f"corr_{date}.h5"
         write_file(fname, self.data, self.header)
         self.reset()
         return fname
-
