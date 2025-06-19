f8f56602cf38c85b329caf6040ab383ad58047de
Christian Hellum Bye <chbye@berkeley.edu>
2025-06-19 16:09:55 -0700

    make panda client autonomous

--- scripts/panda_observe.py
+++ scripts/panda_observe.py
@@ -1,17 +1,15 @@
-import logging
-
 from eigsep_observing import EigsepRedis, PandaClient
 
-LOG_LEVEL = logging.INFO
-PI_IP = "10.10.10.10"
-INIT_TIMEOUT = 60  # seconds to wait for init commands from Redis
-
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=LOG_LEVEL)
-
-redis = EigsepRedis(host=PI_IP)
-client = PandaClient(redis, logger=logger)
-client.read_init_commands(timeout=INIT_TIMEOUT)
+redis = EigsepRedis()
+client = PandaClient(redis)
 
 # main loop, runs indefinitely
-client.read_ctrl()
+while True:
+    try:
+        client.read_ctrl()
+    except KeyboardInterrupt:
+        logger.info("KeyboardInterrupt received, exiting.")
+        break
+
+client.stop_client.set()
+logger.info("Closed connection to Panda.")
