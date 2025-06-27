36d1637ddf4942b47837eaf4c3e064f6144b82d3
Christian Hellum Bye <chbye@berkeley.edu>
2025-06-27 11:58:52 -0700

    add dummy mode

--- scripts/snap_observe.py
+++ scripts/snap_observe.py
@@ -1,11 +1,13 @@
 import logging
 from eigsep_observing.utils import configure_eig_logger
+
 logger = configure_eig_logger(level=logging.DEBUG)
 
 import argparse
 from eigsep_corr.config import load_config
 from eigsep_corr.fpga import add_args
 from eigsep_observing import EigsepFpga
+from eigsep_observing.testing import DummyEigsepFpga
 
 parser = argparse.ArgumentParser(
     description="Snap observing with Eigsep FPGA",
@@ -20,9 +22,13 @@ if args.force_program:
 else:
     program = args.program
 
-snap_ip = cfg["snap_ip"]
-logger.info(f"Connecting to Eigsep correlator at {snap_ip}.")
-fpga = EigsepFpga(cfg=cfg, program=program)
+if args.dummy_mode:
+    logger.warning("Running in DUMMY mode.")
+    fpga = DummyEigsepFpga(cfg=cfg, program=program)
+else:
+    snap_ip = cfg["snap_ip"]
+    logger.info(f"Connecting to Eigsep correlator at {snap_ip}.")
+    fpga = EigsepFpga(cfg=cfg, program=program)
 
 # initialize SNAP
 fpga.initialize(
