7cd88a664df6453aa27419e815ff15100e60f2bf
Christian Hellum Bye <chbye@berkeley.edu>
2025-07-07 12:51:22 -0700

    update tests to match new api

--- src/eigsep_observing/testing/__init__.py
+++ src/eigsep_observing/testing/__init__.py
@@ -1,3 +1,10 @@
 from . import utils
 from .redis import DummyEigsepRedis
 from .fpga import DummyEigsepFpga
+from .pico import (
+    DummyPico,
+    DummyPicoDevice,
+    DummyPicoRFSwitch,
+    DummyPicoPeltier,
+    DummyPicoMotor,
+)
