322671f133fdf9a4efdae40b71b4fb0573f46632
Christian Hellum Bye <chbye@berkeley.edu>
2025-07-08 15:05:55 -0700

    initial commit

---
+++ src/eigsep_observing/testing/client.py
@@ -0,0 +1,15 @@
+from cmt_vna.testing import DummyVNA
+import picohost
+
+from .. import PandaClient
+
+class DummyPandaClient(PandaClient):
+    """
+    Mock up of PandaClient for testing purposes, that uses dummy
+    implementations of the VNA and PicoHost.
+    """
+
+    # override pico classes with dummies
+    PICO_CLASSES = {
+        "imu": None,  # XXX we're here
+    }
