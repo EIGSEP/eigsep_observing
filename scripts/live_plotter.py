ddd35aeaf3e72a41145b2be982d648b4238da0eb
Christian Hellum Bye <chbye@berkeley.edu>
2025-07-08 15:05:35 -0700

    set default redis host

--- scripts/live_plotter.py
+++ scripts/live_plotter.py
@@ -34,7 +34,7 @@ def main():
         default=50,
         help="Polling interval in milliseconds to check for acc_cnt changes",
     )
-    parser.add_argument("--redis-host", default="localhost", help="Redis host")
+    parser.add_argument("--redis-host", default="10.10.10.10", help="Redis host")
     parser.add_argument(
         "--redis-port", type=int, default=6379, help="Redis port"
     )
