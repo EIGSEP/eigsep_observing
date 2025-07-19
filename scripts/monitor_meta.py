04f4e422f219abe790367cced0f8a8526d0c2fff
EIGSEP Pi <chb@berkeley.edu>
2025-07-19 10:20:22 -0700

    monitor lidar

--- scripts/monitor_meta.py
+++ scripts/monitor_meta.py
@@ -7,7 +7,7 @@ r = EigsepRedis("10.10.10.11")
 while True:
     try:
         #m = r.get_live_metadata()
-        m = r.get_live_metadata(keys=["rfswitch", "rfswitch_ts"])
+        m = r.get_live_metadata(keys=["lidar", "lidar_ts"])
         #m = r.get_live_metadata(keys=["imu_panda", "imu_antenna", "lidar"])
         print(json.dumps(m, indent=2, sort_keys=False))
         time.sleep(1.)
